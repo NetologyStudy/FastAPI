23ab3534819f3ef377414905f5858aaf2bdfccdd
practic <maks.mayer.ru@outlook.com>
2025-04-17 16:17:08 +0300

    add hashed_password, replace last migration, change model UsersORM

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,19 +1,29 @@
 from fastapi import APIRouter
 
+from passlib.context import CryptContext
+
 from src.database import async_session_maker
 from src.reposittories.users import UsersRepositories
 from src.schemas.users import UserRequestAdd, UserAdd
 
 router = APIRouter(prefix="/auth", tags=["Авторизация и аутентификация"])
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 @router.post("/register")
 async def register_user(
         data: UserRequestAdd
 ):
-    hashed_password = ""
-    new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
+    hashed_password = pwd_context.hash(data.password)
+    new_user_data = UserAdd(first_name=data.first_name,
+                            last_name=data.last_name,
+                            email=data.email,
+                            hashed_password=hashed_password)
     async with async_session_maker() as session:
-        await UsersRepositories(session).add(new_user_data)
-        await session.commit()
+        try:
+            await UsersRepositories(session).add(new_user_data)
+            await session.commit()
+        except Exception:
+            return "Пользователь с таким email уже существует"
     return {"status": "OK"}
