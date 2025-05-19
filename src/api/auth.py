0809a54036a7cb82dd61b7a9e1de5b03fe23c39f
practic <maks.mayer.ru@outlook.com>
2025-05-19 15:42:53 +0300

    add endpoint "login" and func "create_access_token"

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter
+from datetime import datetime, timedelta, timezone
+from fastapi import APIRouter, HTTPException
 
 from passlib.context import CryptContext
+import jwt
 
 from src.database import async_session_maker
 from src.reposittories.users import UsersRepositories
@@ -9,6 +11,17 @@ from src.schemas.users import UserRequestAdd, UserAdd
 router = APIRouter(prefix="/auth", tags=["Авторизация и аутентификация"])
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode |= {"exp": expire}
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 
 @router.post("/register")
@@ -21,9 +34,18 @@ async def register_user(
                             email=data.email,
                             hashed_password=hashed_password)
     async with async_session_maker() as session:
-        try:
-            await UsersRepositories(session).add(new_user_data)
-            await session.commit()
-        except Exception:
-            return "Пользователь с таким email уже существует"
+        await UsersRepositories(session).add(new_user_data)
+        await session.commit()
     return {"status": "OK"}
+
+
+@router.post("/login")
+async def login_user(
+        data: UserRequestAdd
+):
+    async with async_session_maker() as session:
+        user = await UsersRepositories(session).get_one_or_one(email=data.email)
+        if not user:
+            raise HTTPException(status_code=401, detail="Пользователь с таким email не зарегистрирован")
+        access_token = create_access_token({"user_id": user.id})
+        return {"access_token": access_token}
