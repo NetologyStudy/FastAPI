e399da98e43f6a4555c8ab1b68e50c4fe0ee76b1
practic <maks.mayer.ru@outlook.com>
2025-05-21 19:02:59 +0300

    add endpoint /only_auth

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Response
+from fastapi import APIRouter, HTTPException, Response, Request
 
 from src.database import async_session_maker
 from src.reposittories.users import UsersRepositories
@@ -37,3 +37,11 @@ async def login_user(
         access_token = AuthService().create_access_token({"user_id": user.id})
         response.set_cookie("access_token", access_token)
         return {"access_token": access_token}
+
+
+@router.get("/only_auth")
+async def only_auth(requests: Request):
+    if requests.cookies["access_token"]:
+        return {"access_token": requests.cookies["access_token"]}
+    else:
+        return None
